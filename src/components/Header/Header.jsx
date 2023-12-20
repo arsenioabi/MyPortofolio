1ea037ead6650d10470a12b41bc287349ddbdc81
arsenio abi <76592142+arsenioabi@users.noreply.github.com>
2023-12-21 01:46:43 +0700

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <div className='flex items-center justify-between'>
                     {/*LOGO START*/}
                     <div className='flex items-center gap-[10px]'>
-                        <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center '>
+                        {/*<span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center '>
                             A
                         </span>
 
@@ -16,7 +16,7 @@ const Header = () => {
                             <p className='text-smallTextColor text-[14px] font-[500]'>
                                 personal front end web potrofolio
                             </p>
-                        </div>
+                        </div>*/}
                     </div>
                     {/*LOGO ENDDD*/}
 
@@ -24,16 +24,16 @@ const Header = () => {
                     <div className='menu'>
                         <ul className='flex items-center gap-10'>
                             <li>
-                                <a className='text-smallTextColor font-[600]' href='#about'>About</a>
+                                <a className='text-smallTextColor font-[800] border-b border-solid border-smallTextColor' href='#about'>About</a>
                             </li>
                             <li>
-                                <a className='text-smallTextColor font-[600]' href='#services'>Services</a>
+                                <a className='text-smallTextColor font-[800] border-b border-solid border-smallTextColor' href='#services'>Services</a>
                             </li>
                             <li>
-                                <a className='text-smallTextColor font-[600]' href='#portofolio'>Portofolio</a>
+                                <a className='text-smallTextColor font-[800] border-b border-solid border-smallTextColor' href='#portofolio'>Portofolio</a>
                             </li>
                             <li>
-                                <a className='text-smallTextColor font-[600]' href='#contact'>Contact</a>
+                                <a className='text-smallTextColor font-[800] border-b border-solid border-smallTextColor' href='#contact'>Contact</a>
                             </li>
                         </ul>
                     </div>
@@ -41,9 +41,9 @@ const Header = () => {
 
                     {/*MENU RIGHT*/}
                     <div className='flex items-center gap-4'>
-                        <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300'>
-                            <i class="ri-chat-1-line"></i> Let's Talk
-                        </button>
+                    {/*<button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300'>
+                        <i class="ri-chat-1-line"></i> Let's Talk
+                    </button>*/}
                     </div>
                     {/*MENU ENDDD*/}
                 </div>
